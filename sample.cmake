d65ebd1b2201cc559b5c7e403093a6ce2e6ae214
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-08-09 12:43:47 +0530

    Update sample.cmake

--- sample.cmake
+++ sample.cmake
@@ -1,2 +1,4 @@
 cmake_minimum_required(VERSION 2.6)
-	add_executable(	abstraction abstraction.cpp	)
+	add_executable(	basics basics.c	)
+	add_executable(	header header.h	)
+	add_executable(	storageclass storageclass.c	)
